3378b9fe082135e09fb0c6cd2ae035d044ddcd87
weerayosong <weerayosong@outlook.com>
2026-03-06 13:44:50 +0700

    added js and json files

---
+++ object-and-array.js
@@ -0,0 +1,8 @@
+let learner = {
+    name: "Weerayos",
+    age: 38,
+    isGraduated: false,
+    hobbies: ["Reading","Excercising","Running"]
+};
+let a = `Name: ${learner.name}\nAge: ${learner.age}\nGraduate Status: ${learner.isGraduated}\nHobbies: ${learner.hobbies[0]}`;
+console.log(a);
